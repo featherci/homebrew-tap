994744331d462754fca1178fafebe0473d3d2897
Andy Jeffries <andy@andyjeffries.co.uk>
2026-03-11 13:15:49 +0000

    Add featherci formula v0.1.0

---
+++ Formula/featherci.rb
@@ -0,0 +1,69 @@
+class Featherci < Formula
+  desc "Lightweight self-hosted CI/CD system"
+  homepage "https://github.com/featherci/featherci"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/featherci/featherci/releases/download/v#{version}/featherci-darwin-arm64.tar.gz"
+      sha256 "7c4f2edbdaf47922664b332e761e04922d083414e8741c2c501d92946f1c303f"
+    else
+      url "https://github.com/featherci/featherci/releases/download/v#{version}/featherci-darwin-amd64.tar.gz"
+      sha256 "52a3946a249520ca77b729e213135271ca70940344b5fdf30a21396ef34b7d08"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/featherci/featherci/releases/download/v#{version}/featherci-linux-arm64.tar.gz"
+      sha256 "4ec2eef13c7025346477a1ecd34351098bbd114a90561699630f279d7f0f4634"
+    else
+      url "https://github.com/featherci/featherci/releases/download/v#{version}/featherci-linux-amd64.tar.gz"
+      sha256 "a7d3403f8664eddeb223a1f1195d486a63ea1bfb2fcbbce3c4d508645b9648a5"
+    end
+  end
+
+  depends_on "docker" => :optional
+
+  def install
+    bin.install "featherci"
+    (share/"featherci").install "config.yaml.example"
+  end
+
+  def post_install
+    (var/"featherci").mkpath
+    (var/"log/featherci").mkpath
+  end
+
+  def caveats
+    <<~EOS
+      To start FeatherCI, first generate an encryption key:
+        featherci --generate-key
+
+      Then create a config file at #{etc}/featherci/config.yaml:
+        mkdir -p #{etc}/featherci
+        cp #{opt_share}/featherci/config.yaml.example #{etc}/featherci/config.yaml
+
+      Edit the config file with your settings (secret key, OAuth, admins).
+
+      Start the service:
+        brew services start featherci
+
+      Or run manually:
+        featherci --config #{etc}/featherci/config.yaml
+    EOS
+  end
+
+  service do
+    run [opt_bin/"featherci", "--config", etc/"featherci/config.yaml"]
+    keep_alive true
+    working_dir var/"featherci"
+    log_path var/"log/featherci/featherci.log"
+    error_log_path var/"log/featherci/featherci.error.log"
+  end
+
+  test do
+    assert_match "FeatherCI", shell_output("#{bin}/featherci --version")
+  end
+end
