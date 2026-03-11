d69f0d94d15fff057fb1647bb4c008250e4857d2
Andy Jeffries <andy@andyjeffries.co.uk>
2026-03-11 13:16:32 +0000

    Fix SHA256 hashes to match CI-built release assets

--- Formula/featherci.rb
+++ Formula/featherci.rb
@@ -7,20 +7,20 @@ class Featherci < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/featherci/featherci/releases/download/v#{version}/featherci-darwin-arm64.tar.gz"
-      sha256 "7c4f2edbdaf47922664b332e761e04922d083414e8741c2c501d92946f1c303f"
+      sha256 "0b1d9b61135de3cf4f0e734837cd023634de30392716ad84145dfafa54b53a50"
     else
       url "https://github.com/featherci/featherci/releases/download/v#{version}/featherci-darwin-amd64.tar.gz"
-      sha256 "52a3946a249520ca77b729e213135271ca70940344b5fdf30a21396ef34b7d08"
+      sha256 "3a815eaec57fea242d143048e00ccf3db37e746b02cfb4f9e3859ef557645ed0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/featherci/featherci/releases/download/v#{version}/featherci-linux-arm64.tar.gz"
-      sha256 "4ec2eef13c7025346477a1ecd34351098bbd114a90561699630f279d7f0f4634"
+      sha256 "a7e40882de8088e4bd1b9e714dd25969429584be3dcf62e838eac3134e2de3f0"
     else
       url "https://github.com/featherci/featherci/releases/download/v#{version}/featherci-linux-amd64.tar.gz"
-      sha256 "a7d3403f8664eddeb223a1f1195d486a63ea1bfb2fcbbce3c4d508645b9648a5"
+      sha256 "cbc381a86d416b9090241d4dce9ba95c47677eb33e071e590bc3735b6cad6d77"
     end
   end
 
